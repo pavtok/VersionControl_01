1e6cb2de045a932115de061a93da7d874e10befa
Pablo <pmgiulfo@gmail.com>
2020-09-04 18:31:34 -0300

    First Instructions!

---
+++ VersionControl_01/Instructions.R
@@ -0,0 +1,26 @@
+# ¿Cómo enlazar a GitHub un Proyecto realizado previamente?
+
+# 1) Abrir Git Bash en el directorio en donde se encuentre el Proyecto de R.
+# 2) Escribir los siguientes comandos:
+#     git init
+#     git add .
+      # esto inicializa este directorio como un repositorio Git y 
+      # agrega todos los archivos del directorio al repositorio local.
+#     git commit -m "Initial Commit"
+      # esto commit los cambios al repositorio Git.
+# A este punto hemos creado un Proyecto R y ahora lo tenemos vinculado
+# al control de versiones de Git.
+
+# El próximo paso es vincularlo con GitHub:
+# 3) Ir a github.com y crear un repositorio de igual nombre que nuestro
+# proyecto. 
+# 4) Copiar en Git Bash los comandos que se encuentran en el apartado:
+# "...or push an existing repository from the command line", que son algo similar a:
+#     git remote add origin https://github.com/pavtok/hello-world.git
+#     git branch -M master
+#     git push -u origin master 
+
+
+
+
+
